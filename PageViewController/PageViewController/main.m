76da0717d3f25c0ff5d7563887f3b0c7fcb89686
Umang Arya <umang.arya@payu.in>
2015-12-09 14:51:53 +0530

    Second

---
+++ PageViewController/PageViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PageViewController
+//
+//  Created by Umang Arya on 09/12/15.
+//  Copyright © 2015 PayU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
